7255f43ef0047ff621abc7b0c747b61d7a75bd56
DhruvRana2704 <ddhruvrana@gmail.com>
2024-12-01 12:49:40 +0530

    final_backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 var cors = require('cors')
 connectToMongo()
 const app = express()
-const port = process.env.PORT
+const port = 5000
 app.use(express.json())
 app.use(cors())
 // Available Routes

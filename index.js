26cbd1d83922769b7da408d51c1e27c27bbf82af
DhruvRana2704 <ddhruvrana@gmail.com>
2024-12-01 12:07:05 +0530

    new

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ connectToMongo()
 const app = express()
 const port = 5000
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  {
+    origin:['http://deploy-mern-1whq.vercel.app'],
+    methods:['GET','POST','PUT','DELETE'],
+    credentials:true
+  }
+))
 // Available Routes
 app.get('/', (req, res) => {
   res.send('Hello World!')

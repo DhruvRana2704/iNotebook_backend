2012569fd1a7e1e23e4e4bd37ac5761e2a68c0a5
DhruvRana2704 <ddhruvrana@gmail.com>
2024-12-05 16:21:54 +0530

    sdf

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ var cors = require('cors')
 connectToMongo()
 const app = express()
 const port = process.env.PORT||5000
+const corsOptions = {
+  origin: '*', // Allow all origins (use caution in production)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+};
+
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions));
 // Available Routes
 app.get('/', (req, res) => {
   res.send('Hello World!')

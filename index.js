a6d0209d2c1f7d3623b89d9b69be46dd52d63996
DhruvRana2704 <ddhruvrana@gmail.com>
2024-12-05 16:53:13 +0530

    asd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT||5000
 const corsOptions = {
   origin: '*', // Allow all origins (use caution in production)
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  allowedHeaders: ['Content-Type', 'json'], // Allowed headers
 };
 
 
